41ed21209fe574881ddc897377e47d23bdad6302
Tu Hoang <tuhoangz2k@gmail.com>
2023-03-07 16:47:20 +0700

    responsive mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
 import './resetcss.css';
 import './App.css';
 import Content from './components/Content';
 import SideBar from './components/SideBar';
+import useViewport from './hooks/useViewport';
 
 function App() {
+  const deviceWidth = useViewport().width;
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  useEffect(() => {
+    if (deviceWidth > 768 && isOpenMenu) {
+      setIsOpenMenu(false);
+    }
+  }, [deviceWidth]);
+  const handleCloseMenu = (e) => {
+    setIsOpenMenu(false);
+  };
   return (
     <div className="App">
       <div className="container">
-        <SideBar />
+        <div className="wrap-sidebar">
+          {deviceWidth > 768 ? (
+            <SideBar />
+          ) : (
+            <>
+              <span className="menu" onClick={() => setIsOpenMenu(true)}>
+                <AiOutlineMenu size={32} />
+              </span>
+              {isOpenMenu && (
+                <div className="modal" onClick={handleCloseMenu}>
+                  <SideBar handleCloseMenu={handleCloseMenu} />
+                </div>
+              )}
+            </>
+          )}
+        </div>
         <Content />
       </div>
     </div>
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 import { navigates } from '../../constants';
 import './Sidebar.css';
 import NavLink from '../NavLink';
-export default function SideBar() {
+import useViewport from '../../hooks/useViewport';
+export default function SideBar({ handleCloseMenu }) {
+  const deviceWidth = useViewport().width;
+
   return (
-    <nav className="sidebar">
+    <nav className="sidebar" onClick={(e) => e.stopPropagation()}>
       <ul>
         {navigates.map((ele, idx) => (
           <NavLink href={ele.link} title={ele.title} key={idx} isActive={idx === 0} />
         ))}
       </ul>
+      {deviceWidth <= 768 && (
+        <span className="close-menu" onClick={handleCloseMenu}>
+          <AiOutlineClose size={24} />
+        </span>
+      )}
     </nav>
   );
 }
